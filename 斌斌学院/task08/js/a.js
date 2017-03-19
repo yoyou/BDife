a7ea8b56711088cfd78062a3d6d4b50f18bd2c26
yuyou <yuyou@example.com>
2017-03-19 17:51:21 +0800

    斌斌学院:完成

---
+++ 斌斌学院/task08/js/a.js
@@ -0,0 +1,47 @@
+var prlink;
+prlink=[];
+function init(){
+	var btn = document.getElementsByTagName("button");
+	var che_btn = document.getElementsByName("checkcout");
+	btn[0].addEventListener("click",function(){
+		changeColor(prlink,0);
+		console.log(document.getElementsByTagName('div').length);
+	});	
+	che_btn[0].addEventListener("click",function(){
+		var arr = prlink;
+		var text = document.getElementsByName("searchText")[0].value;
+		for(var i=0;i<arr.length;i++){
+			if(text == arr[i].firstChild.nodeValue.trim()){
+				arr[i].style.backgroundColor="red";
+			}
+		}
+	});
+}
+
+//前序
+function pr_traves(node){
+	var list;
+	list = node.children;
+	if (node != null) {
+		prlink.push(node);
+		for(var i=0;i<list.length;i++){
+			pr_traves(list[i]);
+		}
+	}
+}
+
+function changeColor(arr,index){
+	if (index<arr.length) {
+		if(index != 0){
+			arr[index-1].style.backgroundColor = '#fff';
+		}
+		arr[index].style.backgroundColor = 'red';
+		setTimeout(function(){changeColor(arr,++index)},500);	
+	}else{
+		arr[index-1].style.backgroundColor = '#fff';
+	}
+}
+
+init();
+pr_traves(document.getElementsByTagName('div')[0]);
+
